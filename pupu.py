895394e8f4fb2b85089133c74b5461cc550510a7
innover <innover@gmail.com>
2015-08-30 16:47:16 +0900

    giving up EEE T.T

--- pupu.py
+++ pupu.py
@@ -1,3 +1,3 @@
-o,f,g,s='',' ','*','jB{ExFwHvH?>oSkTkT[CCTXEEQVHFPVHEORS?NPaZcYgViTkRlQnPoNH>J>NNE>?=D=?=LNpOoOoOoMsJvGY=B=OEKAFBQDzDzDzDzExGvIuJsLpQkWb'
-for c in s:o,f,g=o+(ord(c)-59)*f,g,f
+o,f,g='',' ','*'
+for c in 'jB{ExFwHvH?>oSkTkT[CCTXEEQVHFPVHEORS?NPaZcYgViTkRlQnPoNH>J>NNE>?=D=?=LNpOoOoOoMsJvGY=B=OEKAFBQDzDzDzDzExGvIuJsLpQkWb':o,f,g=o+(ord(c)-59)*f,g,f
 for i in range(0,3003,72):print(f*4+o[i:i+72])
